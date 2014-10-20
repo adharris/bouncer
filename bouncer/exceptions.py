1a5c4e5e7129de3f30b1d761e9f3d6273e9b5d21
Andrew <adharris@gmail.com>
2014-10-20 14:45:30 -0400

    Adds user, subject, and action to Access Denied

--- bouncer/exceptions.py
+++ bouncer/exceptions.py
@@ -3,5 +3,14 @@
 
 class AccessDenied(Exception):
     """ This error is raised when a user is not allowed to access a resource
+    
+    :param user: The user who did not have the given ability.
+    :param action: The action that the user did not have.
+    :param subject: The resource that the user was attempting to access.
     """
-    pass
+    
+    def __init__(self, user, action, subject):
+        self.user = user
+        self.action = action
+        self.subject = subject
+
